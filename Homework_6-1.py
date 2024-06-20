8b88b85b683f5bff8fe7641e6dbe3aa1627508fe
VladVasili4 <dipol777@ya.ru>
2024-06-20 07:23:25 +0300

    Add files via upload

---
+++ Homework_6-1.py
@@ -0,0 +1,37 @@
+class Animal:
+    alive = True
+    fed = False
+    def __init__(self, name):
+        self.name = name
+    pass
+
+
+
+
+
+class Plant:
+    edible = False
+    def __init__(self, name):
+        self.name = name
+    pass
+
+
+class Mammal(Animal):
+    def eat(food):
+        
+    pass
+
+
+
+class Predator(Animal):
+    pass
+
+
+class Flower(Plant):
+    pass
+
+
+
+
+class Fruit(Plant):
+    pass
